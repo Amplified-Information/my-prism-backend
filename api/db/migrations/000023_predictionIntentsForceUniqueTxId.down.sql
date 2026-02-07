939bddee8324719dfb9b8d84232d4a4abb51fc9e
Mark Gratton <mark@amplified.info>
2026-02-07 15:25:57 -0800

    Remove project files
    Mass deletion of the repository's project files and subprojects (api, clob, db, eventbus, infra, mw, proxy, web.* , scs, scripts, etc.). Removes source code, configs, Dockerfiles, workflows, assets and related build files to clean up or archive the codebase. If this change was accidental, restore the needed files from the previous commit or branch.

--- api/db/migrations/000023_predictionIntentsForceUniqueTxId.down.sql
+++
@@ -1,3 +0,0 @@
--- roll back the unique constraint on tx_id in prediction_intents table
-ALTER TABLE prediction_intents
-DROP CONSTRAINT unique_tx_id;
--- api/db/migrations/000024_nonNullDescription_markets.up.sql
+++
@@ -1,8 +0,0 @@
--- set description to empty string where it is currently null
-UPDATE markets
-SET description = ''
-WHERE description IS NULL;
-
--- alter the description column to be non-nullable
-ALTER TABLE markets
-ALTER COLUMN description SET NOT NULL;
